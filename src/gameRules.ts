1a22d440af8e14bddebba081c2179bbad0996c7d
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-26 20:02:27 +0100

    Added gameRules

---
+++ src/gameRules.ts
@@ -0,0 +1,11 @@
+type GameRules = {
+	rock: { beats: string };
+	paper: { beats: string };
+	scissors: { beats: string };
+};
+
+const gameRules: GameRules = {
+	rock: { beats: "scissors" },
+	paper: { beats: "rock" },
+	scissors: { beats: "paper" },
+};
