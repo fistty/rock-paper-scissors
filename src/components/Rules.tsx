5f14759a63afb6b89c0bb870509dd5eef94a3c62
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-27 12:36:43 +0100

    Added Framer Motion

--- src/components/Rules.tsx
+++ src/components/Rules.tsx
@@ -1,3 +1,45 @@
-export default function Rules() {
-	return <div>Rules</div>;
+import { motion, useAnimation } from "framer-motion";
+import rulesSvg from "../assets/image-rules.svg";
+import closeSvg from "../assets/icon-close.svg";
+import { useEffect } from "react";
+
+type PropsType = {
+	setIsRulesVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export default function Rules({ setIsRulesVisible }: PropsType) {
+	const closeRulesModal = () => {
+		setIsRulesVisible(false);
+	};
+
+	// Initialize animation controls
+	const controls = useAnimation();
+
+	useEffect(() => {
+		// When the component mounts, start the animation
+		controls.start({ opacity: 1, x: 0 });
+	}, []);
+
+	return (
+		<motion.div
+			id="rules"
+			key="2"
+			initial={{ opacity: 0, x: 500 }}
+			animate={controls}
+			exit={{ opacity: 0, x: 0 }}
+			transition={{ duration: 0.3 }}
+		>
+			<div className="rules-container">
+				<h2>rules</h2>
+				<div className="rules-image-container">
+					<img src={rulesSvg} alt="Rules image" />
+				</div>
+				<div className="rules-close-button-container">
+					<button className="rules-close-button" onClick={closeRulesModal}>
+						<img src={closeSvg} alt="close icon" />
+					</button>
+				</div>
+			</div>
+		</motion.div>
+	);
 }
