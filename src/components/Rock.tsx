bd122fb350b23aaebf90b9d7c55b976561854c2b
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-28 04:45:58 +0100

    Added game card animation

--- src/components/Rock.tsx
+++ src/components/Rock.tsx
@@ -1,8 +1,21 @@
+import { useRef, useEffect } from "react";
 import rockIcon from "../assets/icon-rock.svg";
 
 export default function Rock() {
+	const scissorsRef = useRef<HTMLButtonElement>(null);
+	const animatePosition = () => {
+		scissorsRef.current?.classList.toggle("rock-card-animation");
+	};
+
+	useEffect(() => {
+		scissorsRef.current?.classList.remove("rock-card-amination");
+	}, []);
 	return (
-		<button className="game-card rock-card">
+		<button
+			className="game-card rock-card"
+			onClick={animatePosition}
+			ref={scissorsRef}
+		>
 			<div className="game-card-image-container">
 				<img src={rockIcon} alt="rock icon" />
 			</div>
--- src/components/Scissors.tsx
+++ src/components/Scissors.tsx
@@ -1,8 +1,22 @@
+import { useEffect, useRef } from "react";
 import scissorsIcon from "../assets/icon-scissors.svg";
 
 export default function Scissors() {
+	const scissorsRef = useRef<HTMLButtonElement>(null);
+	const animatePosition = () => {
+		scissorsRef.current?.classList.toggle("scissors-card-animation");
+	};
+
+	useEffect(() => {
+		scissorsRef.current?.classList.remove("scissors-card-amination");
+	}, []);
+
 	return (
-		<button className="game-card scissors-card">
+		<button
+			className="game-card scissors-card"
+			onClick={animatePosition}
+			ref={scissorsRef}
+		>
 			<div className="game-card-image-container">
 				<img src={scissorsIcon} alt="scissors icon" />
 			</div>
