fea68bbdb56ad5d35e5839ed361a7e9bfd78f414
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-28 20:22:29 +0100

    Update Paper.tsx

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,15 +1,34 @@
+import { useEffect, useState } from "react";
 import paperIcon from "../assets/icon-paper.svg";
 import { PropTypes } from "../types";
 
-export default function Paper({ setPlayerPick }: PropTypes) {
+export default function Paper({ playerPick, setPlayerPick }: PropTypes) {
+	const [isParagraph, setIsParagraph] = useState(false);
 	const cardSelection = () => {
 		setPlayerPick("Paper");
 	};
+
+	useEffect(() => {
+		if (playerPick) {
+			const setParagraph = (playerPick: string) => {
+				if (playerPick !== "") {
+					console.log(897782);
+
+					setTimeout(() => {
+						setIsParagraph(true);
+					}, 100);
+				} else return false;
+			};
+			setParagraph(playerPick);
+		}
+	}, [isParagraph, playerPick]);
+
 	return (
 		<button className="game-card paper-card" onClick={cardSelection}>
 			<div className="game-card-image-container">
 				<img src={paperIcon} alt="paper icon" />
 			</div>
+			{isParagraph && <span>You picked THIS</span>}
 		</button>
 	);
 }
