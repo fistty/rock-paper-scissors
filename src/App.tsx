18376f9ed69706df79a15003d7afe0e5d981ccc4
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-28 16:38:38 +0100

    mobile hover fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Rules from "./components/Rules";
-import "./App.css";
 import GameComponent from "./components/GameComponent";
+import "./App.css";
 
 function App() {
 	const [isRulesVisible, setIsRulesVisible] = useState<boolean>(false);
