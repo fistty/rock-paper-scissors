fd15a2cdf772971f3735846920fb5c21fc24dffc
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-29 17:54:21 +0100

    Update gameRules.ts

--- src/gameRules.ts
+++ src/gameRules.ts
@@ -1,11 +1,11 @@
-// type GameRules = {
-// 	rock: { beats: string };
-// 	paper: { beats: string };
-// 	scissors: { beats: string };
-// };
+type GameRules = {
+	rock: { beats: string };
+	paper: { beats: string };
+	scissors: { beats: string };
+};
 
-// const gameRules: GameRules = {
-// 	rock: { beats: "scissors" },
-// 	paper: { beats: "rock" },
-// 	scissors: { beats: "paper" },
-// };
+export const gameRules: GameRules = {
+	rock: { beats: "scissors" },
+	paper: { beats: "rock" },
+	scissors: { beats: "paper" },
+};
