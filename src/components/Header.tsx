84e64f10cafdd69232a059ecaf5c8fcc7f927de6
Osuntuyi Amos <osuntuyiemilos@gmail.com>
2023-10-31 06:03:23 +0100

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 				<div className="game-logo-container">
 					<img className="game-logo" src={logo} alt="Rock Paper Scissors Logo" />
 				</div>
-				<Score count={0} />
+				<Score count={578} />
 			</div>
 		</header>
 	);
